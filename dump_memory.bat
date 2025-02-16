197badd2b51517c831650c172ac5676c0290fe94
Encryptize <46298681+Encryptize@users.noreply.github.com>
2025-02-16 01:02:35 +0100

    fix typo in dump_memory script

--- dump_memory.bat
+++ dump_memory.bat
@@ -1,7 +1,7 @@
 @echo off
 openocd -s oocd/scripts -f oocd/scripts/interface/stlink.cfg -f oocd/scripts/target/at32.cfg -c "init" -c "dump_image MEMORY_ZT3Pro.bin 0x08000000 0x0803FFFF" -c "exit"
 IF EXIST MEMORY_ZT3Pro.bin (
-	zt3tool.exe MEMORY_ZT3Pro_modded.bin verify
+	zt3tool.exe MEMORY_ZT3Pro.bin verify
 ) ELSE (
 	echo MEMORY_ZT3Pro.bin missing.
 )
