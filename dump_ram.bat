f1fea2c1f5e680ac1044e0a5a1e23b99ab6505fa
Amirabbas <amirabasalinaghi@gmail.com>
2025-05-05 11:50:37 +0200

    name change

--- dump_ram.bat
+++ dump_ram.bat
@@ -1,3 +1,3 @@
 @echo off
-openocd -s oocd/scripts -f oocd/scripts/interface/stlink.cfg -f oocd/scripts/target/at32.cfg -c "init" -c "dump_image RAM_MaxG3_dumped.bin 0x20000000 0x7D00" -c "exit"
+openocd -s oocd/scripts -f oocd/scripts/interface/stlink.cfg -f oocd/scripts/target/at32.cfg -c "init" -c "dump_image RAM_G3_dumped.bin 0x20000000 0x7D00" -c "exit"
 pause
--- flash_memory.bat
+++ flash_memory.bat
@@ -1,7 +1,7 @@
 @echo off
-IF EXIST MEMORY_MaxG3_modded.bin (
-	openocd -s oocd/scripts -f oocd/scripts/interface/stlink.cfg -f oocd/scripts/target/at32.cfg -c "init" -c "reset halt" -c "flash probe 0" -c "stm32f1x unlock 0" -c "reset halt" -c "stm32f1x mass_erase 0" -c "flash write_bank 0 MEMORY_MaxG3_modded.bin" -c "reset run" -c "exit"
+IF EXIST MEMORY_G3_modded.bin (
+	openocd -s oocd/scripts -f oocd/scripts/interface/stlink.cfg -f oocd/scripts/target/at32.cfg -c "init" -c "reset halt" -c "flash probe 0" -c "stm32f1x unlock 0" -c "reset halt" -c "stm32f1x mass_erase 0" -c "flash write_bank 0 MEMORY_G3_modded.bin" -c "reset run" -c "exit"
 ) ELSE (
-	echo MEMORY_MaxG3_modded.bin missing.
+	echo MEMORY_G3_modded.bin missing.
 )
 pause
