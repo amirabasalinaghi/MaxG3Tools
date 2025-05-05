00a9e26173003bb9a77b53858c0750a39691c6a7
Amirabbas <amirabasalinaghi@gmail.com>
2025-05-05 10:40:22 +0200

    change files names for g3

--- dump_memory.bat
+++ dump_memory.bat
@@ -1,8 +1,8 @@
 @echo off
-openocd -s oocd/scripts -f oocd/scripts/interface/stlink.cfg -f oocd/scripts/target/at32.cfg -c "init" -c "dump_image MEMORY_ZT3Pro.bin 0x08000000 0x0803FFFF" -c "exit"
-IF EXIST MEMORY_ZT3Pro.bin (
-	zt3tool.exe MEMORY_ZT3Pro.bin verify
+openocd -s oocd/scripts -f oocd/scripts/interface/stlink.cfg -f oocd/scripts/target/at32.cfg -c "init" -c "dump_image MEMORY_MaxG3.bin 0x08000000 0x0803FFFF" -c "exit"
+IF EXIST MEMORY_MaxG3.bin (
+	zt3tool.exe MEMORY_MaxG3.bin verify
 ) ELSE (
-	echo MEMORY_ZT3Pro.bin missing.
+	echo MEMORY_MaxG3.bin missing.
 )
 pause
